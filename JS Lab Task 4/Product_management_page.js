ad19c04bfe1e09f67cd5c7d0b31d85624e03cc42
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-03-16 02:07:28 +0600

    Finished all the task

--- JS Lab Task 4/Product_management_page.js
+++ JS Lab Task 4/Product_management_page.js
@@ -2,29 +2,84 @@ let items = document.getElementById("Items");
 let quantity = document.getElementById("quantity");
 let submit = document.getElementById("submit");
 let totalCost = document.getElementById("totalCost");
-let costPerItem = 1000; // Assuming a fixed cost per item for simplicity
+let itemPrices = 1000; 3// fixed unit
 
-submit.addEventListener("click", check);
+    submit.addEventListener("click", check);
 
 function check(event) 
 {
+
+    event.preventDefault();
+    
+    
+    let selectedValue = items.value;
     let quantityValue = quantity.value;
-    if (quantityValue === "")
+    
+    if (selectedValue === "") 
     {
-        event.preventDefault();
+        alert("Please select an item!");
+    } 
+    else if (quantityValue === "")
+    {
         alert("Please enter a quantity!");
         return;
     }
+    else if (quantityValue < 0)
+    {
+        alert("Quantity cannot be negative!");
+        return;
+    }
     
-    let selectedValue = items.value;
+   
+   let total = itemPrices * quantityValue;
+    totalCost.value = "$" + total.toFixed(2);
+    
+    console.log("Total calculated:", total);
 
+    if (total > 1000)
+    {
+        alert("You are now eligible for gift coupon");
+    }
+    
+
+
+}
+//extra credit task
+let checkListButton = document.getElementById("Check list");
+
+checkListButton.addEventListener("click", confirmList);
+function confirmList() 
+{
+    let selectedValue = items.value;
+    let quantityValue = quantity.value;
+    
     if (selectedValue === "") 
     {
-        event.preventDefault();
         alert("Please select an item!");
     } 
-    else 
+    else if (quantityValue === "")
     {
-        alert("You selected: " + selectedValue);
+        alert("Please enter a quantity!");
+        return;
+    }
+    else if (quantityValue < 0)
+    {
+        alert("Quantity cannot be negative!");
+        return;
+    }
+    else
+    {        
+        confirm("Your selection is: " + selectedValue + " with quantity: " + quantityValue);
+    }
+   
+   let total = itemPrices * quantityValue;
+    totalCost.value = "$" + total.toFixed(2);
+    confirm("Total calculated: $" + total.toFixed(2));
+    console.log("Total calculated:", total);
+
+    if (total > 1000)
+    {
+        alert("You are now eligible for gift coupon");
     }
 }
+
